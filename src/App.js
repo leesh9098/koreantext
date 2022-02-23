76751e277d744c076dce07ae6697b36cfc663c1b
leesh9098 <leesh9098@naver.com>
2022-02-23 13:01:55 +0800

    22.02.23

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import { MoeumData } from './data/MoeumData';
 
 function App() {
   const [text, setText] = useState("");
-  let [test, setTest] = useState("");
-  const [csnumber, SetCsNumber] = useState("");
-  const [t, setT] = useState("");
+  const [separate1, setSeparate1] = useState("");
+  const [separate2, setSeparate2] = useState("");
+  const [separate3, setSeparate3] = useState("");
+  const [separate4, setSeparate4] = useState("");
+  const [csnumber1, SetCsNumber1] = useState("");
+  const [csnumber2, SetCsNumber2] = useState("");
+  const [csnumber3, SetCsNumber3] = useState("");
+  const [csnumber4, SetCsNumber4] = useState("");
+  const [strokeNumber1, setStrokeNumber1] = useState("");
+  const [strokeNumber2, setStrokeNumber2] = useState("");
+  const [strokeNumber3, setStrokeNumber3] = useState("");
+  const [strokeNumber4, setStrokeNumber4] = useState("");
 
   // let code;
 
@@ -18,32 +27,55 @@ function App() {
 
   const getConstantVowel = () => {
     if (text !== "") {
-    //   for(let i = 0; i < text.length; i++) {
-    //     code = text.charCodeAt(i)-44032;
-    //     if(code > -1 && code < 11172) {
-    //       setTest(test += JaeumData[Math.floor(code/588)].jaeum);
-    //     } else {
-    //       setTest(test += text.charAt(i));
-    //     };
-    //   }
-    //   return code;
-    // } else {
-    //   setTest("");
-    // }
-    // for(let i = 0; i < text.length; i++) {
       const ga = 44032;
-      let uni = text.charCodeAt(0);
+      let uni1 = text.charCodeAt(0);
+      let uni2 = text.charCodeAt(1);
+      let uni3;
+      let uni4;
+      if (text.length === 3) {
+        uni3 = text.charCodeAt(2) - ga;
+        let fn3 = parseInt(uni3 / 588);
+        let sn3 = parseInt((uni3 - (fn3 * 588)) / 28);
+        let tn3 = parseInt(uni3 % 28);
+        setSeparate3(JaeumData[fn3].jaeum + MoeumData[sn3].moeum + Jongseong[tn3].jongseong);
+        SetCsNumber3(`초성=${JaeumData[fn3].id}, 모음=${MoeumData[sn3].id}, 받침=${Jongseong[tn3].id}`);
+        setStrokeNumber3(`초성=${JaeumData[fn3].number}, 모음=${MoeumData[sn3].number} 받침=${Jongseong[tn3].number}`);
+      }
+      if (text.length === 4) {
+        uni3 = text.charCodeAt(2) - ga;
+        uni4 = text.charCodeAt(3) - ga;
+        let fn3 = parseInt(uni3 / 588);
+        let sn3 = parseInt((uni3 - (fn3 * 588)) / 28);
+        let tn3 = parseInt(uni3 % 28);
+        let fn4 = parseInt(uni4 / 588);
+        let sn4 = parseInt((uni4 - (fn4 * 588)) / 28);
+        let tn4 = parseInt(uni4 % 28);
+        setSeparate3(JaeumData[fn3].jaeum + MoeumData[sn3].moeum + Jongseong[tn3].jongseong);
+        SetCsNumber3(`초성=${JaeumData[fn3].id}, 모음=${MoeumData[sn3].id}, 받침=${Jongseong[tn3].id}`);
+        setStrokeNumber3(`초성=${JaeumData[fn3].number}, 모음=${MoeumData[sn3].number} 받침=${Jongseong[tn3].number}`);
+        setSeparate4(JaeumData[fn4].jaeum + MoeumData[sn4].moeum + Jongseong[tn4].jongseong);
+        SetCsNumber4(`초성=${JaeumData[fn4].id}, 모음=${MoeumData[sn4].id}, 받침=${Jongseong[tn4].id}`);
+        setStrokeNumber4(`초성=${JaeumData[fn4].number}, 모음=${MoeumData[sn4].number} 받침=${Jongseong[tn4].number}`);
+      }
 
-      uni = uni - ga;
+      uni1 = uni1 - ga;
+      uni2 = uni2 - ga;
 
-      let fn = parseInt(uni / 588)
-      let sn = parseInt((uni - (fn * 588)) / 28)
-      let tn = parseInt(uni % 28);
-      setTest(test += JaeumData[fn].jaeum + MoeumData[sn].moeum + Jongseong[tn].jongseong);
-      SetCsNumber(`초성=${JaeumData[fn].id}, 모음=${MoeumData[sn].id}, 받침=${Jongseong[tn].id}`)
-      setT(`초성=${JaeumData[fn].number}, 모음=${MoeumData[sn].number} 받침=${Jongseong[tn].number}`);
+      let fn1 = parseInt(uni1 / 588);
+      let fn2 = parseInt(uni2 / 588);
+      let sn1 = parseInt((uni1 - (fn1 * 588)) / 28);
+      let sn2 = parseInt((uni2 - (fn2 * 588)) / 28);
+      let tn1 = parseInt(uni1 % 28);
+      let tn2 = parseInt(uni2 % 28);
+      setSeparate1(JaeumData[fn1].jaeum + MoeumData[sn1].moeum + Jongseong[tn1].jongseong);
+      setSeparate2(JaeumData[fn2].jaeum + MoeumData[sn2].moeum + Jongseong[tn2].jongseong);
+      SetCsNumber1(`초성=${JaeumData[fn1].id}, 모음=${MoeumData[sn1].id}, 받침=${Jongseong[tn1].id}`);
+      SetCsNumber2(`초성=${JaeumData[fn2].id}, 모음=${MoeumData[sn2].id}, 받침=${Jongseong[tn2].id}`);
+      setStrokeNumber1(`초성=${JaeumData[fn1].number}, 모음=${MoeumData[sn1].number} 받침=${Jongseong[tn1].number}`);
+      setStrokeNumber2(`초성=${JaeumData[fn2].number}, 모음=${MoeumData[sn2].number} 받침=${Jongseong[tn2].number}`);
+      
     } else {
-      alert("입력란에 입력해주세요")
+      alert("입력란에 입력해주세요");
     }
   }
 
@@ -55,16 +87,81 @@ function App() {
 
   return (
     <div className="App">
-      <input type="text" id='input' onChange={handleInput} value={text} onKeyPress={PressEnter} />
+      <input
+        type="text"
+        id='input'
+        onChange={handleInput}
+        value={text}
+        onKeyPress={PressEnter}
+        maxLength="4" />
       <button onClick={getConstantVowel}>확인</button>
       <p>
-        자모음 분리: {test}
+        첫 번째 글자 자모음 분리: {separate1}
       </p>
       <p>
-        초성의 순번: {csnumber}
+        두 번째 글자 자모음 분리: {separate2}
       </p>
       <p>
-        각 글자의 획수: {t}
+        {
+          text.length > 2 ?
+          `세 번째 글자 자모음 분리: ${separate3}`
+          : 
+          null
+        }
+      </p>
+      <p>
+        {
+          text.length > 3 ?
+          `네 번째 글자 자모음 분리: ${separate4}`
+          : 
+          null
+        }
+      </p>
+      <hr />
+      <p>
+        첫 번째 글자 초성의 순번: {csnumber1}
+      </p>
+      <p>
+        두 번째 글자 초성의 순번: {csnumber2}
+      </p>
+      <p>
+        {
+          text.length > 2 ?
+          `세 번째 글자 초성의 순번: ${csnumber3}`
+          :
+          null
+        }
+      </p>
+      <p>
+        {
+          text.length > 3 ?
+          `네 번째 글자 초성의 순번: ${csnumber4}`
+          :
+          null
+        }
+      </p>
+      <hr />
+      <p>
+        첫 번째 글자의 획수: {strokeNumber1}
+      </p>
+      <p>
+        두 번째 각 글자의 획수: {strokeNumber2}
+      </p>
+      <p>
+        {
+          text.length > 2 ?
+          `세 번째 각 글자의 획수: ${strokeNumber3}`
+          :
+          null
+        }
+      </p>
+      <p>
+        {
+          text.length > 3 ?
+          `네 번째 각 글자의 획수: ${strokeNumber4}`
+          :
+          null
+        }
       </p>
     </div>
   );
--- src/data/Jongseong.jsx
+++ src/data/Jongseong.jsx
@@ -1,142 +1,142 @@
 export const Jongseong = [
     {
         "jongseong": "",
-        "id": 1,
+        "id": 0,
         "number": 0
     },
     {
         "jongseong": "ㄱ",
-        "id": 2,
+        "id": 1,
         "number": 2
     },
     {
         "jongseong": "ㄲ",
-        "id": 3,
+        "id": 2,
         "number": 4
     },
     {
         "jongseong": "ㄳ",
-        "id": 4,
+        "id": 3,
         "number": 4
     },
     {
         "jongseong": "ㄴ",
-        "id": 5,
+        "id": 4,
         "number": 2
     },
     {
         "jongseong": "ㄵ",
-        "id": 6,
+        "id": 5,
         "number": 5
     },
     {
         "jongseong": "ㄶ",
-        "id": 7,
+        "id": 6,
         "number": 5
     },
     {
         "jongseong": "ㄷ",
-        "id": 8,
+        "id": 7,
         "number": 3
     },
     {
         "jongseong": "ㄹ",
-        "id": 9,
+        "id": 8,
         "number": 5
     },
     {
         "jongseong": "ㄺ",
-        "id": 10,
+        "id": 9,
         "number": 7
     },
     {
         "jongseong": "ㄻ",
-        "id": 11,
+        "id": 10,
         "number": 9
     },
     {
         "jongseong": "ㄼ",
-        "id": 12,
+        "id": 11,
         "number": 9
     },
     {
         "jongseong": "ㄽ",
-        "id": 13,
+        "id": 12,
         "number": 7
     },
     {
         "jongseong": "ㄾ",
-        "id": 14,
+        "id": 13,
         "number": 8
     },
     {
         "jongseong": "ㄿ",
-        "id": 15,
+        "id": 14,
         "number": 9
     },
     {
         "jongseong": "ㅀ",
-        "id": 16,
+        "id": 15,
         "number": 8
     },
     {
         "jongseong": "ㅁ",
-        "id": 17,
+        "id": 16,
         "number": 4
     },
     {
         "jongseong": "ㅂ",
-        "id": 18,
+        "id": 17,
         "number": 4
     },
     {
         "jongseong": "ㅄ",
-        "id": 19,
+        "id": 18,
         "number": 6
     },
     {
         "jongseong": "ㅅ",
-        "id": 20,
+        "id": 19,
         "number": 2
     },
     {
         "jongseong": "ㅆ",
-        "id": 21,
+        "id": 20,
         "number": 4
     },
     {
         "jongseong": "ㅇ",
-        "id": 22,
+        "id": 21,
         "number": 1
     },
     {
         "jongseong": "ㅈ",
-        "id": 23,
+        "id": 22,
         "number": 3
     },
     {
         "jongseong": "ㅊ",
-        "id": 24,
+        "id": 23,
         "number": 4
     },
     {
         "jongseong": "ㅋ",
-        "id": 25,
+        "id": 24,
         "number": 3
     },
     {
         "jongseong": "ㅌ",
-        "id": 26,
+        "id": 25,
         "number": 4
     },
     {
         "jongseong": "ㅍ",
-        "id": 27,
+        "id": 26,
         "number": 4
     },
     {
         "jongseong": "ㅎ",
-        "id": 28,
+        "id": 27,
         "number": 3
     },
 ]
